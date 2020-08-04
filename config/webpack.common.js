933cc2d9523fc72663524b8fc6ce50be1019d39f
程俊杰 <chengjunjie@meituan.com>
2020-08-04 21:15:38 +0800

    允许console

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -71,8 +71,8 @@ module.exports = {
         // 在uplifyJS删除没有用到的代码时不输出警告
         warnings: false,
         compress: {
-          // 删除所以的console.log语句，可以兼容ie浏览器
-          drop_console: true,
+          // 不删除所有的console.log语句，可以兼容ie浏览器
+          drop_console: false,
           // 内嵌定义了但是只用到一次的变量
           collapse_vars: true,
           // 提取出现多次但是没有定义成变量去引用的静态值
--- src/components/m1.js
+++ src/components/m1.js
@@ -1,1 +1,1 @@
-export const M1_NAME = 'm1';
+export const M1_NAME = 'm2';
